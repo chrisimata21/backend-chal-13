8a26349aab7a7c0afffe3c8847cb15e6cf0119b1
chrisimata21 <chrisimata21@gmail.com>
2022-11-10 20:10:29 -0800

    defined columns in Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns: ID is a unique value given to each record and increments by 1 for each new record created. Acts as key value.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
